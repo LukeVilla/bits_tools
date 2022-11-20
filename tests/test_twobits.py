487a39e0f0e9687efa6319253f351ff9ee8bf965
LukeVilla <lukevlobos@gmail.com>
2022-11-20 13:21:13 -0800

    I think it's ready for release

---
+++ tests/test_twobits.py
@@ -0,0 +1,35 @@
+import sys
+import pytest
+
+def test_twobits():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.bits == [0,1]
+def test_twobits_and():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.andGate() == 0
+def test_twobits_or():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.orGate() == 1
+def test_twobits_not():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.notGate() == [1,0]
+def test_twobits_nand():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.nandGate() == 1
+def test_twobits_nor():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.norGate() == 0
+def test_twobits_xor():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.xorGate() == 1
+def test_twobits_xnor():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.xnorGate() == 0

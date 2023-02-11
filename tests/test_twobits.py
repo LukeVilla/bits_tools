484354f21a1da5a9f204f592780f7cfaf72bc37c
LukeVilla <lukevlobos@gmail.com>
2023-02-11 12:02:24 -0800

    Finished tests for TwoBits

--- tests/test_twobits.py
+++ tests/test_twobits.py
@@ -1,7 +1,7 @@
 import sys
 import pytest
 
-def test_twobits():
+def test_twobits_tools():
     import bits
     zero_one = bits.TwoBits(0,1)
     assert zero_one.bits == [0,1]
@@ -29,7 +29,32 @@ def test_twobits_xor():
     import bits
     zero_one = bits.TwoBits(0,1)
     assert zero_one.xorGate() == 1
-def test_twobits_xnor():
+def test_twobits_tools_xnor():
     import bits
     zero_one = bits.TwoBits(0,1)
     assert zero_one.xnorGate() == 0
+def test_validate_good():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    try:
+        zero_one.validate([0,1])
+        assert True
+    except:
+        assert False
+def test_validate_bad():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    try:
+        zero_one.validate([0,2])
+        assert False
+    except:
+        assert True
+def test_setbits():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    zero_one.set_bits([1,0])
+    assert zero_one.bits == [1,0]
+def test_getbits():
+    import bits
+    zero_one = bits.TwoBits(0,1)
+    assert zero_one.get_bits() == [0,1]

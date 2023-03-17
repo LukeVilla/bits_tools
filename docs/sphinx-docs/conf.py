ffd0ea05a3baf2a0fcf025731935fbfcb8694f2c
LukeVilla <lukevlobos@gmail.com>
2023-03-17 16:10:22 -0700

    First run of Sphinx

---
+++ docs/sphinx-docs/conf.py
@@ -0,0 +1,8 @@
+project = "bits-tools"
+author = "Luke Villalobos"
+copyright = "2023, Luke Villalobos"
+version = "1.1"
+release = "1.1"
+rst_epilog = """.. role:: python(code)
+   :language: python"""
+root_doc = "README"

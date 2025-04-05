86afdc77d9230e0b86f6d60e2c21bc3dd653b837
LukeVilla <lukevlobos@gmail.com>
2025-04-05 10:01:25 -0700

    Fixed version number

--- docs/conf.py
+++ docs/conf.py
@@ -1,8 +1,8 @@
 project = "bits-tools"
 author = "Luke Villalobos"
 copyright = "2025, Luke Villalobos"
-version = "1.1"
-release = "1.1"
+version = "2.0"
+release = "2.0"
 rst_epilog = """.. role:: python(code)
    :language: python"""
 root_doc = "index"
